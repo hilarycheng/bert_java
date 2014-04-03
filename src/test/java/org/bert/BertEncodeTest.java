742803e694b9ee59856f99cacd69726e56160e24
Hilary Cheng <hilary@polayer.com>
2014-04-03 16:06:38 +0800

    - Add Encode Test

---
+++ src/test/java/org/bert/BertEncodeTest.java
@@ -0,0 +1,23 @@
+package org.bert;
+
+import org.bert.Bert.Atom;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.rules.ExpectedException;
+import java.util.Arrays;
+import java.util.Date;
+
+@RunWith(JUnit4.class)
+public class BertEncodeTest {
+
+	@Test
+	public void encodeAtom() {
+	}
+	
+}
