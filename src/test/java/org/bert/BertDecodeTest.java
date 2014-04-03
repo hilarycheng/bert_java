e3ad36b3c47e5df5c49b0b78047313bbd10b629e
Hilary Cheng <hilary@polayer.com>
2014-04-03 16:05:29 +0800

    - Rename to BertDecodeTest

--- src/test/java/org/bert/BertDecodeTest.java
+++ src/test/java/org/bert/BertDecodeTest.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.Date;
 
 @RunWith(JUnit4.class)
-public class BertTest {
+public class BertDecodeTest {
 
 	@Test(expected = BertException.class)
     public void testInvalid() throws BertException {
